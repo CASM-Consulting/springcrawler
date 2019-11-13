e79dc9a221c04e94af033a8e3bb3fc78d6e2c369
Simon Wibberley <simon.wibberley@gmail.com>
2019-11-13 13:36:24 +0000

    non-caputuring match

--- src/main/java/com/casm/acled/crawler/spring/SpringCrawler.java
+++ src/main/java/com/casm/acled/crawler/spring/SpringCrawler.java
@@ -100,7 +100,7 @@ public class SpringCrawler implements CommandLineRunner {
         ImporterConfig ic = new ImporterConfig();
 
         EmptyMetadataFilter emptyArticle = new EmptyMetadataFilter(OnMatch.EXCLUDE,ACLEDScraperPreProcessor.SCRAPEDJSON);
-        String regex = ".+(kill|massacre|death|died|dead|bomb|bombed|bombing|rebel|attack|attacked|riot|battle|protest|clash|demonstration|strike|wound|injure|casualty|displace|unrest|casualties|vigilante|torture|march|rape).+";
+        String regex = ".+(?:kill|massacre|death|died|dead|bomb|bombed|bombing|rebel|attack|attacked|riot|battle|protest|clash|demonstration|strike|wound|injure|casualty|displace|unrest|casualties|vigilante|torture|march|rape).+";
         RegexMetadataFilter regexFilter = new RegexMetadataFilter(ACLEDScraperPreProcessor.SCRAPEDJSON,regex);
         ic.setPostParseHandlers(emptyArticle,regexFilter);
 
