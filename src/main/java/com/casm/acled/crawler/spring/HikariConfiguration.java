25f5da33e45066a52b5ba6d7e16294d50cbcd948
Jack <jp242@sussex.ac.uk>
2019-11-19 14:31:50 +0000

    trying to resolve hikari issue

--- src/main/java/com/casm/acled/crawler/spring/HikariConfiguration.java
+++ src/main/java/com/casm/acled/crawler/spring/HikariConfiguration.java
@@ -3,13 +3,11 @@ package com.casm.acled.crawler.spring;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.SearchStrategy;
 import org.springframework.boot.autoconfigure.jmx.ParentAwareNamingStrategy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableMBeanExport;
-import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.*;
 import org.springframework.core.env.Environment;
 import org.springframework.jmx.export.annotation.AnnotationJmxAttributeSource;
 import org.springframework.jmx.export.naming.ObjectNamingStrategy;
@@ -21,20 +19,49 @@ import java.util.UUID;
 
 //@EnableMBeanExport(registration= RegistrationPolicy.IGNORE_EXISTING)
 @Configuration
+@PropertySource(value= {"classpath:application.properties"})
 public class HikariConfiguration extends HikariConfig {
 
     @Autowired
     Environment environment;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverName;
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String userName;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+//    @Bean
+//    @Primary
+//    public DataSource dataSource() throws SQLException {
+//        HikariDataSource dataSource = new HikariDataSource(this);
+//        dataSource.setPoolName("dataSource_" + UUID.randomUUID().toString());
+//        dataSource.setJdbcUrl("jdbc:postgresql://127.0.0.1:5432/postgres");
+//        dataSource.setUsername("postgres");
+//        dataSource.setPassword("postgres");
+//        dataSource.setDriverClassName("org.postgresql.Driver");
+//        return dataSource;
+//    }
 
     @Bean
     @Primary
     public DataSource dataSource() throws SQLException {
         HikariDataSource dataSource = new HikariDataSource(this);
         dataSource.setPoolName("dataSource_" + UUID.randomUUID().toString());
-        dataSource.setJdbcUrl("jdbc:postgresql://127.0.0.1:5432/postgres");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("postgres");
-        dataSource.setDriverClassName("org.postgresql.Driver");
+//        dataSource.setJdbcUrl("jdbc:postgresql://127.0.0.1:5432/postgres");
+//        dataSource.setUsername("postgres");
+//        dataSource.setPassword("postgres");
+//        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setJdbcUrl(url);
+        dataSource.setDriverClassName(driverName);
+        dataSource.setUsername(userName);
+        dataSource.setPassword(password);
         return dataSource;
     }
 
