cd75dc5bcbeceebb19462505f79f7f1775b95d60
Jack <jp242@sussex.ac.uk>
2019-11-19 15:26:06 +0000

    more hikari flim flam

--- src/main/java/com/casm/acled/crawler/spring/HikariConfiguration.java
+++ src/main/java/com/casm/acled/crawler/spring/HikariConfiguration.java
@@ -1,52 +1,52 @@
-package com.casm.acled.crawler.spring;
-
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.SearchStrategy;
-import org.springframework.boot.autoconfigure.jmx.ParentAwareNamingStrategy;
-import org.springframework.context.annotation.*;
-import org.springframework.core.env.Environment;
-import org.springframework.jmx.export.annotation.AnnotationJmxAttributeSource;
-import org.springframework.jmx.export.naming.ObjectNamingStrategy;
-import org.springframework.jmx.support.RegistrationPolicy;
-
-import javax.sql.DataSource;
-import java.sql.SQLException;
-import java.util.UUID;
-
-//@EnableMBeanExport(registration= RegistrationPolicy.IGNORE_EXISTING)
-@Configuration
-//@PropertySource(value= {"classpath:application.properties"})
-public class HikariConfiguration extends HikariConfig {
-
-    private static HikariConfig config = new HikariConfig();
-
-    {
-        config.setJdbcUrl("jdbc:postgresql://127.0.0.1:5432/postgres");
-        config.setUsername("postgres");
-        config.setPassword("postgres");
-        config.setDriverClassName("org.postgresql.Driver");
-    }
-
-    @Autowired
-    Environment environment;
-
-    @Bean
-    @Primary
-    public DataSource dataSource() throws SQLException {
-        HikariDataSource dataSource = new HikariDataSource(config);
-        dataSource.setPoolName("dataSource_" + UUID.randomUUID().toString());
-        return dataSource;
-    }
-
-    @Bean
-    @ConditionalOnMissingBean(value = ObjectNamingStrategy.class, search = SearchStrategy.CURRENT)
-    public ParentAwareNamingStrategy objectNamingStrategy() {
-        ParentAwareNamingStrategy namingStrategy = new ParentAwareNamingStrategy(new AnnotationJmxAttributeSource());
-        namingStrategy.setDefaultDomain("domain_" + UUID.randomUUID().toString());
-        return namingStrategy;
-    }
-}
+//package com.casm.acled.crawler.spring;
+//
+//import com.zaxxer.hikari.HikariConfig;
+//import com.zaxxer.hikari.HikariDataSource;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+//import org.springframework.boot.autoconfigure.condition.SearchStrategy;
+//import org.springframework.boot.autoconfigure.jmx.ParentAwareNamingStrategy;
+//import org.springframework.context.annotation.*;
+//import org.springframework.core.env.Environment;
+//import org.springframework.jmx.export.annotation.AnnotationJmxAttributeSource;
+//import org.springframework.jmx.export.naming.ObjectNamingStrategy;
+//import org.springframework.jmx.support.RegistrationPolicy;
+//
+//import javax.sql.DataSource;
+//import java.sql.SQLException;
+//import java.util.UUID;
+//
+////@EnableMBeanExport(registration= RegistrationPolicy.IGNORE_EXISTING)
+//@Configuration
+////@PropertySource(value= {"classpath:application.properties"})
+//public class HikariConfiguration extends HikariConfig {
+//
+//    private static HikariConfig config = new HikariConfig();
+//
+//    {
+//        config.setJdbcUrl("jdbc:postgresql://127.0.0.1:5432/postgres");
+//        config.setUsername("postgres");
+//        config.setPassword("postgres");
+//        config.setDriverClassName("org.postgresql.Driver");
+//    }
+//
+//    @Autowired
+//    Environment environment;
+//
+//    @Bean
+//    @Primary
+//    public DataSource dataSource() throws SQLException {
+//        HikariDataSource dataSource = new HikariDataSource(config);
+//        dataSource.setPoolName("dataSource_" + UUID.randomUUID().toString());
+//        return dataSource;
+//    }
+//
+//    @Bean
+//    @ConditionalOnMissingBean(value = ObjectNamingStrategy.class, search = SearchStrategy.CURRENT)
+//    public ParentAwareNamingStrategy objectNamingStrategy() {
+//        ParentAwareNamingStrategy namingStrategy = new ParentAwareNamingStrategy(new AnnotationJmxAttributeSource());
+//        namingStrategy.setDefaultDomain("domain_" + UUID.randomUUID().toString());
+//        return namingStrategy;
+//    }
+//}
