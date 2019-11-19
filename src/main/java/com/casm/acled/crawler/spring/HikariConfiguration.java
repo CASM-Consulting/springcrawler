6176da5faa4b4c70e38c7132d53c9903bda3e126
Jack <jp242@sussex.ac.uk>
2019-11-19 14:40:54 +0000

    trying to resolve hikari issue

--- src/main/java/com/casm/acled/crawler/spring/HikariConfiguration.java
+++ src/main/java/com/casm/acled/crawler/spring/HikariConfiguration.java
@@ -24,46 +24,46 @@ public class HikariConfiguration extends HikariConfig {
 
     @Autowired
     Environment environment;
-
-    @Value("${spring.datasource.driver-class-name}")
-    private String driverName;
-
-    @Value("${spring.datasource.url}")
-    private String url;
-
-    @Value("${spring.datasource.username}")
-    private String userName;
-
-    @Value("${spring.datasource.password}")
-    private String password;
-
-//    @Bean
-//    @Primary
-//    public DataSource dataSource() throws SQLException {
-//        HikariDataSource dataSource = new HikariDataSource(this);
-//        dataSource.setPoolName("dataSource_" + UUID.randomUUID().toString());
-//        dataSource.setJdbcUrl("jdbc:postgresql://127.0.0.1:5432/postgres");
-//        dataSource.setUsername("postgres");
-//        dataSource.setPassword("postgres");
-//        dataSource.setDriverClassName("org.postgresql.Driver");
-//        return dataSource;
-//    }
+//
+//    @Value("${spring.datasource.driver-class-name}")
+//    private String driverName;
+//
+//    @Value("${spring.datasource.url}")
+//    private String url;
+//
+//    @Value("${spring.datasource.username}")
+//    private String userName;
+//
+//    @Value("${spring.datasource.password}")
+//    private String password;
 
     @Bean
     @Primary
     public DataSource dataSource() throws SQLException {
         HikariDataSource dataSource = new HikariDataSource(this);
         dataSource.setPoolName("dataSource_" + UUID.randomUUID().toString());
-//        dataSource.setJdbcUrl("jdbc:postgresql://127.0.0.1:5432/postgres");
-//        dataSource.setUsername("postgres");
-//        dataSource.setPassword("postgres");
-//        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setJdbcUrl(url);
-        dataSource.setDriverClassName(driverName);
-        dataSource.setUsername(userName);
-        dataSource.setPassword(password);
+        dataSource.setJdbcUrl("jdbc:postgresql://127.0.0.1:5432/postgres");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("postgres");
+        dataSource.setDriverClassName("org.postgresql.Driver");
         return dataSource;
     }
+
+//    @Bean
+//    @Primary
+//    public DataSource dataSource() throws SQLException {
+//        HikariDataSource dataSource = new HikariDataSource(this);
+//        dataSource.setPoolName("dataSource_" + UUID.randomUUID().toString());
+////        dataSource.setJdbcUrl("jdbc:postgresql://127.0.0.1:5432/postgres");
+////        dataSource.setUsername("postgres");
+////        dataSource.setPassword("postgres");
+////        dataSource.setDriverClassName("org.postgresql.Driver");
+//        dataSource.setJdbcUrl(url);
+//        dataSource.setDriverClassName(driverName);
+//        dataSource.setUsername(userName);
+//        dataSource.setPassword(password);
+//        return dataSource;
+//    }
 
     @Bean
     @ConditionalOnMissingBean(value = ObjectNamingStrategy.class, search = SearchStrategy.CURRENT)
