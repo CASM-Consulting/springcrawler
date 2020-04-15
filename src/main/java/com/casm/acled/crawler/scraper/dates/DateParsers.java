9d617e6d1c2d00f079e852291ea5fe20a4187734
Simon Wibberley <simon.wibberley@gmail.com>
2020-04-15 14:29:20 +0100

    switched to variable delimited format

--- src/main/java/com/casm/acled/crawler/scraper/dates/DateParsers.java
+++ src/main/java/com/casm/acled/crawler/scraper/dates/DateParsers.java
@@ -5,12 +5,12 @@ import com.google.common.collect.ImmutableList;
 public class DateParsers {
 
     public static final DateParser dp1 = CompositeDateParser.of(ImmutableList.of(
-            "ISO:MMMM d, yyyy:en",
-            "ISO:MMMM d, yyyy, HH:mm a:en",
-            "ISO:dd MMM yyyy:en",
-            "ISO:dd.MM.yyyy:en",
-            "ISO:ddF-MMM-yyyy:en",
-            "ISO:ddF-MMM-yyyy:en,ORD",
-            "ISO:dd MMM yyyy HH:mm zzz:en,RE" //Published: 26 Nov 2019 01:43 AM BdST Updated: 26 Nov 2019 01:44 AM BdST
+            "ISO:/MMMM d, yyyy/en",
+            "ISO:/MMMM d, yyyy, HH:mm a/en",
+            "ISO:/dd MMM yyyy/en",
+            "ISO:/dd.MM.yyyy/en",
+            "ISO:/ddF-MMM-yyyy/en",
+            "ISO:/ddF-MMM-yyyy/en/ORD",
+            "ISO:/dd MMM yyyy HH:mm a z/en_GB/BST/RE.*Updated: (.*)" //Published: 26 Nov 2019 01:43 AM BdST Updated: 26 Nov 2019 01:44 AM BdST
     ));
 }
