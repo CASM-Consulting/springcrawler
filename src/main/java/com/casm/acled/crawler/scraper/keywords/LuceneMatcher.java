95196e2b1f87d7f90c14546a4a8ee4ff3e7e31ec
Simon Wibberley <simon.wibberley@gmail.com>
2020-05-13 11:42:20 +0100

    beginnings of lucene match explanation

---
+++ src/main/java/com/casm/acled/crawler/scraper/keywords/LuceneMatcher.java
@@ -0,0 +1,63 @@
+package com.casm.acled.crawler.scraper.keywords;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.core.SimpleAnalyzer;
+import org.apache.lucene.index.memory.MemoryIndex;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.Explanation;
+import org.apache.lucene.search.Query;
+
+import java.io.IOException;
+
+public class LuceneMatcher {
+
+    private String queryConfig;
+    private final Analyzer analyzer;
+    private final Query query;
+    private final String FIELD = "field";
+
+    public LuceneMatcher(String queryConfig) {
+        analyzer = new SimpleAnalyzer();
+
+        QueryParser parser = new QueryParser(FIELD, analyzer);
+
+        try {
+            query = parser.parse(queryConfig);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public boolean isMatched(String text) {
+
+        MemoryIndex index = new MemoryIndex();
+        index.addField(FIELD, text, analyzer);
+        float score = index.search(query);
+
+        try {
+
+            Explanation explanation = index.createSearcher().explain(query,0);
+
+            int c = 0;
+        } catch (IOException e) {
+            //pass
+        }
+//
+//        QueryTermExtractor
+//        IndexSearcher searcher = new IndexSearcher(directory);
+//        TopDocs topDocs = searcher.search(query, 10);
+//        for (int i = 0; i < topDocs.totalHits; i++) {
+//            ScoreDoc match = topDocs.scoreDocs[i];
+//            Explanation explanation = searcher.explain(query, match.doc);
+//            System.out.println("----------");
+//            Document doc = searcher.doc(match.doc);
+//            System.out.println(doc.get("title"));
+//            System.out.println(explanation.toString());
+//        }
+
+        return score > 0.0f;
+    }
+
+}
